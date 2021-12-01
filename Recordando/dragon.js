bb2dc4f6c4d57c77f337198e8d68a369b1b3bb99
kleymerDP <dpkleymer@gmail.com>
2021-12-01 02:38:44 -0500

    Se puso en practica lo aprendido y he analizado el Debug del codigo para poder comprenderlo mejor

--- Recordando/dragon.js
+++
@@ -1,6 +0,0 @@
-const API_REQ = 'https://jsonplaceholder.typicode.com/post';
-const app = document.querySelector('#app');
-
-fetch(API_REQ)
-    .then( res => res.json() )
-    .then( data => console.log(data) )
---
+++ poniendoEnPractica.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        "id": "ZM2HMskVfKQty3wXW6yOb0V",
+        "productName": "Secadora de Pelo",
+        "cost": 520
+    },
+    {
+        "id": "m7wvyl9SS0reMHMd0PYRsBu",
+        "productName": "Refrigeradora",
+        "cost": 2230
+    },
+    {
+        "id": "NgeeNEZ9MvEYorJRoP7fiLE",
+        "productName": "Televisión 50\"",
+        "cost": 1500
+    },
+    {
+        "id": "aVv9Ck8nY0r4JQKbKdNQQNV",
+        "productName": "Secadora de Pelo",
+        "cost": 350
+    }
+];
+
+// console.log(producto);
+
+const disconuntedProducts = products.map(function(product) {
+    if (product.cost < 1000) return product;
+    return {
+        ...product,
+        cost: product.cost * 0.8 
+    }
+});
+
+console.log(disconuntedProducts);
